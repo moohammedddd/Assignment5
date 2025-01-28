3deba58274cdde7e651c9b7dbed4e1bae1a72496
moohammedddd <midorafat115566@gmail.com>
2025-01-28 05:41:44 +0200

    ThirdProject

--- Assignment5/Program.cs
+++ Assignment5/Program.cs
@@ -48,6 +48,78 @@
             Console.WriteLine($"{Math.Div(a, b)}");
             #endregion
 
+            #region ThirdProject
+            Duration d1 = new Duration(1, 2, 3);
+            Console.WriteLine(d1);
+            Duration d2 = new Duration(330);
+            Console.WriteLine(d2);
+            #region D3=D1+D2
+            Console.WriteLine("after adding");
+            Duration d3 = d1 + d2;
+            Console.WriteLine(d3);
+            #endregion
+
+            #region D3=D1 + 7800
+            Console.WriteLine("after adding secound");
+            Duration d4 = d1 + 7800;
+            Console.WriteLine(d4.ToString());
+            #endregion
+
+            #region D3=666+D3
+            Duration d5 = 666 + d3;
+            Console.WriteLine(d5);
+            #endregion
+
+            #region D3= ++D1 (Increase One Minute)
+            Duration d6 = ++d1;
+            Console.WriteLine(d6);
+
+
+            #endregion
+
+            #region D3 = --D2 (Decrease One Minute)
+            Duration d7 = --d6;
+            Console.WriteLine(d7);
+            #endregion
+
+            #region D1= D1 -D2
+            Duration d8 = d1 - d2;
+            Console.WriteLine(d8);
+            #endregion
+
+            #region If (D1>D2)
+            if (d1 > d2)
+            {
+                Console.WriteLine("d2 > d1");
+            }
+            else
+            {
+                Console.WriteLine("d1 < d2");
+            }
+            #endregion
+
+            #region If (D1<=D2)
+            if (d1 <= d2)
+            {
+                Console.WriteLine("d1 <= d2");
+            }
+            else
+            {
+                Console.WriteLine("d1 >= d2");
+            }
+            #endregion
+
+            #region If (D1)
+            if (d2)
+                Console.WriteLine("d2 is true");
+            #endregion
+
+            #region DateTime Obj = (DateTime) D1
+            DateTime obj = (DateTime)d1;
+            Console.WriteLine(obj);
+            #endregion
+
+            #endregion
         }
     }
 }
