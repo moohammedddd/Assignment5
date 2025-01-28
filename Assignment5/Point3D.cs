66b1db9986432c1e1f42739c61e5d95ff542b689
moohammedddd <midorafat115566@gmail.com>
2025-01-28 05:36:00 +0200

    FirstProject

---
+++ Assignment5/Point3D.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    internal class Point3D
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+        public Point3D(int _x, int _y, int _z)
+        {
+            X = _x;
+            Y = _y;
+            Z = _z;
+        }
+
+        override public string ToString()
+        {
+            return $" Point Coordinates: ({X}, {Y}, {Z})";
+        }
+
+        public static bool operator ==(Point3D p1, Point3D p2)
+        {
+            return p1?.X == p2?.X && p1?.Y == p2?.Y && p1?.Z == p2?.Z;
+
+        }
+
+        public static bool operator !=(Point3D p1, Point3D p2)
+        {
+            //if (p1?.X != p2?.X && p1?.Y != p2?.Y && p1?.Z != p2?.Z)
+            //{
+            return p1?.X != p2?.X && p1?.Y != p2?.Y && p1?.Z != p2?.Z;
+            //}
+
+        }
+
+
+
+    }
+}
--- Assignment5/Program.cs
+++ Assignment5/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region FirstProject
+            Point3D P1 = new Point3D(10, 20, 30);
+            Console.WriteLine(P1);
+            Console.WriteLine("enter the x point");
+            int x1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the y point");
+            int y1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the z point");
+            int z1 = int.Parse(Console.ReadLine());
+            Point3D P2 = new Point3D(x1, y1, z1);
+            Console.WriteLine(P2);
+            if (P2 == P1)
+            {
+                Console.WriteLine("The Two Point is Equal");
+            }
+            else
+            {
+                Console.WriteLine("The Two Point is Not Equal");
+            }
+
+            Point3D[] points =
+            {
+               new Point3D (10,20,30),
+              new Point3D  (5,20,30),
+              new Point3D  (16,30,50)
+            };
+            //Array.Sort(points);
+            for (int i = 0; i < points.Length; i++)
+            {
+                Console.WriteLine(points[i]);
+            }
+
+
+            #endregion
         }
     }
 }
